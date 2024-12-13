cdaf15d579177c299c08cb02cfb1b7ac540ebf0c
PrinMax <max.prin@edu.univ-fcomte.fr>
2024-12-13 14:48:56 +0100

    changes

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,22 +2,26 @@ const menuToggle = document.querySelector('.menu-toggle');
 const overlayMenu = document.querySelector('.overlay-menu');
 const closeMenu = document.querySelector('.close-menu');
 
+// Basculer l'affichage du menu
 menuToggle.addEventListener('click', () => {
-    overlayMenu.classList.add('show');
+    overlayMenu.classList.toggle('show');
 });
 
+// Fermer le menu avec le bouton de fermeture
 closeMenu.addEventListener('click', () => {
     overlayMenu.classList.remove('show');
 });
 
 const menuLinks = document.querySelectorAll('.menu-link');
 
+// Gérer le clic sur les liens du menu
 menuLinks.forEach(link => {
     link.addEventListener('click', (event) => {
         event.preventDefault();
         const pageName = link.getAttribute('data-link');
         console.log(`Lien cliqué : ${pageName}`);
         const url = link.getAttribute('href');
+        overlayMenu.classList.remove('show'); // Fermer le menu après un clic
         window.location.href = url;
     });
 });
